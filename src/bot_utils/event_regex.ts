5fa5964d85cf4bfabaa052d40121b9f5426b1cd9
Arghya Chanda <arghyac35@gmail.com>
2020-06-19 17:06:05 +0530

    Fix regex for to check for only dot & slash
    * Bot started responding to every commands done with any symbols at first with last regex, blame my poor testing for this issue
    * Also fix list command when COMMANDS_USE_BOT_NAME is enabled after adding the new regex
    
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>

--- src/bot_utils/event_regex.ts
+++ src/bot_utils/event_regex.ts
@@ -7,13 +7,13 @@ export class EventRegex {
   readonly commandsRegexNoName: regexps.RegExps;
 
   constructor() {
-    var commands = ['^/(?= )|^.start', '^/(?= )|^.mirrorTar', '^/(?= )|^.mirror', '^/(?= )|^.mirrorStatus', '^/(?= )|^.list', '^/(?= )|^.getFolder', '^/(?= )|^.cancelMirror', '^/(?= )|^.cancelAll', '^/(?= )|^.disk', '^/(?= )|^.getLink', '^/(?= )|^.clone', '^/(?= )|^.id'];
+    var commands = ['^[/|.]start', '^[/|.]mirrorTar', '^[/|.]mirror', '^[/|.]mirrorStatus', '^[/|.]list', '^[/|.]getFolder', '^[/|.]cancelMirror', '^[/|.]cancelAll', '^[/|.]disk', '^[/|.]getLink', '^[/|.]clone', '^[/|.]id'];
     var commandsNoName: string[] = [];
     var commandAfter = ['$', ' (.+)', ' (.+)', '$', ' (.+)', '$', '$', '$', '$', ' (.+)', ' (.+)', '$'];
 
     if (constants.COMMANDS_USE_BOT_NAME && constants.COMMANDS_USE_BOT_NAME.ENABLED) {
       commands.forEach((command, i) => {
-        if (command === '^/list') {
+        if (command === '^[/|.]list') {
           // In case of more than one of these bots in the same group, we want all of them to respond to /list
           commands[i] = command + commandAfter[i];
         } else {
