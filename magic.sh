66b448635ea32998c35c8cfcbd9d8c6786ca2b49
Arghya Chanda <arghyac35@gmail.com>
2020-09-05 16:56:36 +0000

    Exclude downloads in magis.sh
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>

--- magic.sh
+++ magic.sh
@@ -1,4 +1,4 @@
-tar --exclude='./node_modules' --exclude='./.git' -cvf 69.tar .
+tar --exclude='./node_modules' --exclude='./.git' --exclude='./downloads' -cvf 69.tar .
 git add 69.tar
 git commit -m "69"
 git push heroku master --force
