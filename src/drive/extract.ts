51c17b9179adb76f6a92b42b952c9a783fa2f65d
Arghya Chanda <arghyac35@gmail.com>
2020-09-21 11:11:35 +0530

    Revert rar in unzipmirror
    * Will look later
    
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>

--- src/drive/extract.ts
+++ src/drive/extract.ts
@@ -1,7 +1,6 @@
 const inly = require('inly');
 import fs = require('fs');
 import getSize from 'get-folder-size';
-import { createExtractorFromFile } from 'node-unrar-js';
 import extractZip from 'extract-zip';
 
 export function extract(srcPath: string, fileName: string, ext: string, callback: (err: string, size: number, realFilePath: string) => void): void {
@@ -11,74 +10,49 @@ export function extract(srcPath: string, fileName: string, ext: string, callback
         fs.mkdirSync(dlDirPath, { recursive: true });
     }
 
-    switch (ext) {
-        case 'zip':
-            extractZip(srcPath, { dir: dlDirPath }).then(() => {
-                getSize(dlDirPath, (err, size) => {
-                    if (fs.existsSync(dlDirPath + '/' + fileName)) {
-                        dlDirPath = dlDirPath + '/' + fileName;
-                    }
-                    if (err) {
-                        console.log('Couldn\'t determine file size: ', err.message);
-                        callback(err.message, null, dlDirPath);
-                    } else {
-                        callback(null, size, dlDirPath);
-                    }
-                });
-            }).catch(err => callback(err, null, null));
-            break;
-        case 'rar':
-            // Read the archive file
-            const extractor = createExtractorFromFile(srcPath, dlDirPath);
-            const extracted = extractor.extractAll();
-
-            if (extracted[0].state === 'SUCCESS') {
-                getSize(dlDirPath, (err, size) => {
-                    if (fs.existsSync(dlDirPath + '/' + fileName)) {
-                        dlDirPath = dlDirPath + '/' + fileName;
-                    }
-                    if (err) {
-                        console.log('Couldn\'t determine file size: ', err.message);
-                        callback(err.message, null, dlDirPath);
-                    } else {
-                        callback(null, size, dlDirPath);
-                    }
-                });
-            } else {
-                console.error('Extracting rar failed: ', extracted[0].reason);
-                callback(extracted[0].msg, null, null);
-            }
-            break;
-        default:
-            const extractProcess = inly(srcPath, dlDirPath);
-
-            extractProcess.on('file', (name: any) => {
-                console.log(name);
+    if (ext === 'zip') {
+        extractZip(srcPath, { dir: dlDirPath }).then(() => {
+            getSize(dlDirPath, (err, size) => {
+                if (fs.existsSync(dlDirPath + '/' + fileName)) {
+                    dlDirPath = dlDirPath + '/' + fileName;
+                }
+                if (err) {
+                    console.log('Couldn\'t determine file size: ', err.message);
+                    callback(err.message, null, dlDirPath);
+                } else {
+                    callback(null, size, dlDirPath);
+                }
             });
+        }).catch(err => callback(err, null, null));
+    } else {
+        const extractProcess = inly(srcPath, dlDirPath);
 
-            extractProcess.on('progress', (percent: string) => {
-                console.log(percent + '%');
-            });
+        extractProcess.on('file', (name: any) => {
+            console.log(name);
+        });
 
-            extractProcess.on('error', (error: any) => {
-                console.error(error);
-                callback(error, null, null);
-            });
+        extractProcess.on('progress', (percent: string) => {
+            console.log(percent + '%');
+        });
 
-            extractProcess.on('end', () => {
-                getSize(dlDirPath, (err, size) => {
-                    if (fs.existsSync(dlDirPath + '/' + fileName)) {
-                        dlDirPath = dlDirPath + '/' + fileName;
-                    }
-                    if (err) {
-                        console.log('Couldn\'t determine file size: ', err.message);
-                        callback(err.message, null, dlDirPath);
-                    } else {
-                        callback(null, size, dlDirPath);
-                    }
-                });
+        extractProcess.on('error', (error: any) => {
+            console.error(error);
+            callback(error, null, null);
+        });
+
+        extractProcess.on('end', () => {
+            getSize(dlDirPath, (err, size) => {
+                if (fs.existsSync(dlDirPath + '/' + fileName)) {
+                    dlDirPath = dlDirPath + '/' + fileName;
+                }
+                if (err) {
+                    console.log('Couldn\'t determine file size: ', err.message);
+                    callback(err.message, null, dlDirPath);
+                } else {
+                    callback(null, size, dlDirPath);
+                }
             });
-            break;
+        });
     }
 }
 
