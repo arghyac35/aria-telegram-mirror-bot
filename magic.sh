c80a04466101b3fe0eb058991c45ee8e330e3d3d
nenokkadine <65813999+nenokkadine@users.noreply.github.com>
2021-01-19 00:15:21 +0530

    init

--- magic.sh
+++
@@ -1,4 +0,0 @@
-tar --exclude='./node_modules' --exclude='./.git' --exclude='./downloads' -cvf 69.tar .
-git add 69.tar
-git commit -m "69"
-git push heroku master --force
--- start.sh
+++ start.sh
@@ -1,1 +1,26 @@
-npm install && ./aria.sh && npm start
+#!/bin/bash
+
+if [[ -n $SA_ZIP_URL ]]; then
+	echo "Usage of Service Accounts Detected"
+	wget -q $ SA_ZIP_URL -O accounts.zip && unzip -qq accounts.zip
+elif [[ -n $TOKEN_URL && -n $CRED_JSON]]; then
+	echo "Usage of token detected"
+	wget -q CREDENTIALS -O credentials.json
+	wget -q CLIENT_SECRET -O client_secrect.json
+else
+	echo "Neither Service Accounts Nor Token Provided. Exiting..."
+	exit 0
+fi
+
+if [[ -n $CONFIG_URL ]]; then
+	wget -q $CONFIG_URL -O /app/out/.constants.js
+else
+	echo "Provide constants.js to Run the bot. Exiting..."
+	exit 0
+tracker_list=`curl -Ns https://raw.githubusercontent.com/XIU2/TrackersListCollection/master/all.txt | awk '$1' | tr '\n' ',' | cat`
+if [[ -n $MAX_CONCURRENT_DOWNLOADS ]]; then
+	echo -e "\nmax-concurrent-downloads=$MAX_CONCURRENT_DOWNLOADS\nbt-tracker=$tracker_list" >> /app/aria.conf
+else
+	echo -e "\nmax-concurrent-downloads=3\nbt-tracker=$tracker_list" >> /app/aria.conf
+aria2c --conf-path=aria.conf
+npm start
