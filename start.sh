c7b2aa669dece23419e13f38495d407ed6905ccf
nenokkadine <65813999+nenokkadine@users.noreply.github.com>
2021-01-19 10:32:56 +0530

    Ahh F

--- start.sh
+++ start.sh
@@ -17,6 +17,7 @@ if [[ -n $CONFIG_URL ]]; then
 else
 	echo "Provide constants.js to Run the bot. Exiting..."
 	exit 0
+fi
 tracker_list=`curl -Ns https://raw.githubusercontent.com/XIU2/TrackersListCollection/master/all.txt | awk '$1' | tr '\n' ',' | cat`
 if [[ -n $MAX_CONCURRENT_DOWNLOADS ]]; then
 	echo -e "\nmax-concurrent-downloads=$MAX_CONCURRENT_DOWNLOADS\nbt-tracker=$tracker_list" >> /app/aria.conf
