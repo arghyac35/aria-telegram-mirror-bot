c4e4393909396cd095191f111c9da875076f6010
nenokkadine <65813999+nenokkadine@users.noreply.github.com>
2021-01-19 15:48:38 +0530

    Fix Deploy script

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-if [[ -n "${{ secrets.HEROKU_EMAIL }}" && -n "${{ secrets.HEROKU_API_KEY }}" ]]; then
-	sed -Ei "s/login/login "${{secrets.HEROKU_EMAIL}}"/g" .netrc
-	sed -Ei "s/password/password "${{secrets.HEROKU_API_KEY}}"/g" .netrc
+if [[ -n "$HEROKU_EMAIL" && -n "$HEROKU_API_KEY" ]]; then
+	sed -Ei "s/login/login "$HEROKU_EMAIL"/g" .netrc
+	sed -Ei "s/password/password "$HEROKU_API_KEY"/g" .netrc
 	mv .netrc ~/.netrc
 else
 	echo "Heroku Credentials Not Found, Add them in secrets"
 	exit 2
 fi
 
-if [[ -n ${ secrets.REGION } && -n ${ secrets.HEROKU_APP } ]]; then
+if [[ -n "$REGION" && -n "$HEROKU_APP" ]]; then
 	heroku container:login
 	echo "Creating App"
-	heroku apps:create ${ secrets.HEROKU_APP } --stack=container --region=eu
+	heroku apps:create "$HEROKU_APP" --stack=container --region=eu
 	if [[ $? -eq 0 ]]; then
 		echo "Successfully created app"
 	else
@@ -19,13 +19,13 @@ if [[ -n ${ secrets.REGION } && -n ${ secrets.HEROKU_APP } ]]; then
 		exit 2
 	fi
 	echo "Building and pushing the app to Heroku Registry"
-	heroku container:push worker -a ${ secrets.HEROKU_APP }
+	heroku container:push worker -a "$HEROKU_APP"
 	echo "Deploying"
-	heroku container:release worker -a ${ secrets.HEROKU_APP }
-elif [[ -n ${ secrets.HEROKU_APP } ]]; then
+	heroku container:release worker -a "$HEROKU_APP"
+elif [[ -n "$HEROKU_APP" ]]; then
 	heroku container:login
 	echo "Creating App"
-	heroku apps:create ${ secrets.HEROKU_APP } --stack=container
+	heroku apps:create "$HEROKU_APP" --stack=container
 	if [[ $? -eq 0 ]]; then
 		echo "Successfully created app"
 	else
@@ -33,9 +33,9 @@ elif [[ -n ${ secrets.HEROKU_APP } ]]; then
 		exit 2
 	fi
 	echo "Building and pushing the app to Heroku Registry"
-	heroku container:push worker -a ${ secrets.HEROKU_APP }
+	heroku container:push worker -a "$HEROKU_APP"
 	echo "Deploying"
-	heroku container:release worker -a ${ secrets.HEROKU_APP }
+	heroku container:release worker -a "$HEROKU_APP"
 else 
 	echo "Heroku App name Not Provided"
 fi
@@ -44,16 +44,16 @@ echo "Deployment Success"
 
 echo "Setting Config Vars"
 
-if [[ -n ${ secrets.SA_ZIP_URL } && -n ${ secrets.CONFIG_URL } ]]; then
-	heroku config:set -a ${ secrets.HEROKU_APP } SA_ZIP_URL=${ secrets.SA_ZIP_URL } CONFIG_URL=${ secrets.CONFIG_URL } 
-elif [[ -n ${ secrets.CLIENT_SECRET } && -n ${ secrets.CREDENTIALS } && -n ${ secrets.CONFIG_URL } ]]; then
-	heroku config:set -a ${ secrets.HEROKU_APP } CONFIG_URL=${ secrets.CONFIG_URL } CREDENTIALS=${ secrets.CREDENTIALS } CLIENT_SECRET=${ secrets.CLIENT_SECRET }
+if [[ -n "$SA_ZIP_URL" && -n "$CONFIG_URL" ]]; then
+	heroku config:set -a "$HEROKU_APP" SA_ZIP_URL="$SA_ZIP_URL" CONFIG_URL="$CONFIG_URL" 
+elif [[ -n "$CLIENT_SECRET" && -n "$CREDENTIALS" && -n "$CONFIG_URL" ]]; then
+	heroku config:set -a "$HEROKU_APP" CONFIG_URL="$CONFIG_URL" CREDENTIALS="$CREDENTIALS" CLIENT_SECRET="$CLIENT_SECRET"
 else
 	echo "Config error Check Secrets For Reference check README"
 	exit 2
 	
-if [[ -n ${ secrets.MAX_CONCURRENT_DOWNLOADS } ]]; then
-	heroku config:set -a ${ secrets.HEROKU_APP } MAX_CONCURRENT_DOWNLOADS=${ secrets.MAX_CONCURRENT_DOWNLOADS }
+if [[ -n "$MAX_CONCURRENT_DOWNLOADS" ]]; then
+	heroku config:set -a "$HEROKU_APP" } MAX_CONCURRENT_DOWNLOADS="$MAX_CONCURRENT_DOWNLOADS"
 else
 	echo "Max Concurrent Downloads Var Not given so Defaults to 3"
 fi
