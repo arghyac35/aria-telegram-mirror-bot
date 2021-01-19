31b2c14d9e950e542c6503b31b928236440fc430
Arghya Chanda <arghyac35@gmail.com>
2021-01-19 19:00:49 +0530

    Fix start script
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>

--- start.sh
+++ start.sh
@@ -2,11 +2,11 @@
 
 if [[ -n $SA_ZIP_URL ]]; then
 	echo "Usage of Service Accounts Detected"
-	wget -q $ SA_ZIP_URL -O accounts.zip && unzip -qq accounts.zip
+	wget -q $SA_ZIP_URL -O /app/accounts.zip && unzip -qq -o accounts.zip
 elif [[ -n $CLIENT_SECRET && -n $CREDENTIALS ]]; then
 	echo "Usage of token detected"
-	wget -q CREDENTIALS -O credentials.json
-	wget -q CLIENT_SECRET -O client_secrect.json
+	wget -q $CREDENTIALS -O /app/credentials.json
+	wget -q $CLIENT_SECRET -O /app/client_secret.json
 else
 	echo "Neither Service Accounts Nor Token Provided. Exiting..."
 	exit 0
@@ -17,10 +17,13 @@ if [[ -n $CONFIG_URL ]]; then
 else
 	echo "Provide constants.js to Run the bot. Exiting..."
 	exit 0
+fi
 tracker_list=`curl -Ns https://raw.githubusercontent.com/XIU2/TrackersListCollection/master/all.txt | awk '$1' | tr '\n' ',' | cat`
 if [[ -n $MAX_CONCURRENT_DOWNLOADS ]]; then
 	echo -e "\nmax-concurrent-downloads=$MAX_CONCURRENT_DOWNLOADS\nbt-tracker=$tracker_list" >> /app/aria.conf
 else
 	echo -e "\nmax-concurrent-downloads=3\nbt-tracker=$tracker_list" >> /app/aria.conf
+fi
+
 aria2c --conf-path=aria.conf
 npm start
