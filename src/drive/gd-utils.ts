707442b5649bde31647578d71835ba8215da45ab
Arghya Chanda <arghyac35@gmail.com>
2021-01-19 19:48:46 +0530

    Fix my own silly bugs fml
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>

--- src/drive/gd-utils.ts
+++ src/drive/gd-utils.ts
@@ -58,8 +58,8 @@ export async function real_copy(source: string, target: string, tg?: any) {
     console.log('Number of folders to be copied：', folders.length)
     console.log('Number of files to be copied：', files.length)
 
-    if (folders.length === 0 || files.length) {
-        throw new Error("No files/folder found for copying.");
+    if (files.length === 0) {
+        throw new Error("No files found for copying.");
     }
 
     const mapping = await create_folders(
