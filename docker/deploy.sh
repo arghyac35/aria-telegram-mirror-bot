79f1a82ca491390d655998a5e8b546fe4411b8da
nenokkadine <65813999+nenokkadine@users.noreply.github.com>
2021-01-19 13:49:40 +0530

    Update deploy.sh

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -13,9 +13,8 @@ else
 	exit 2
 fi
 
-heroku container:login
-#docker login --username=_ --password=$(heroku auth:token) registry.heroku.com
 if [[ -n ${{secrets.REGION}} && -n ${{secrets.HEROKU_APP}} ]]; then
+	heroku container:login
 	echo "Creating App"
 	heroku apps:create ${{secrets.HEROKU_APP}} --stack=container --region=eu
 	if [[ $? -eq 0 ]]; then
@@ -29,6 +28,7 @@ if [[ -n ${{secrets.REGION}} && -n ${{secrets.HEROKU_APP}} ]]; then
 	echo "Deploying"
 	heroku container:release worker -a ${{secrets.HEROKU_APP}}
 elif [[ -n ${{secrets.HEROKU_APP}} ]]; then
+	heroku container:login
 	echo "Creating App"
 	heroku apps:create ${{secrets.HEROKU_APP}} --stack=container
 	if [[ $? -eq 0 ]]; then
@@ -52,10 +52,15 @@ echo "Setting Config Vars"
 if [[ -n ${{secrets.SA_ZIP_URL}} && -n ${{secrets.CONFIG_URL}} ]]; then
 	heroku config:set -a ${{secrets.HEROKU_APP}} SA_ZIP_URL=${{secrets.SA_ZIP_URL}} CONFIG_URL=${{secrets.CONFIG_URL}} 
 elif [[ -n ${{secrets.CLIENT_SECRET}} && -n ${{secrets.CREDENTIALS}} && -n ${{secrets.CONFIG_URL}} ]]; then
-	heroku config:set -a ${{secrets.HEROKU_APP}} CONFIG_URL=${{secrets.CONFIG_URL}} CREDENTIALS=${{secrets.CREDENTIALS}} CLIENT_SECRET: ${{secrets.CLIENT_SECRET}}
+	heroku config:set -a ${{secrets.HEROKU_APP}} CONFIG_URL=${{secrets.CONFIG_URL}} CREDENTIALS=${{secrets.CREDENTIALS}} CLIENT_SECRET=${{secrets.CLIENT_SECRET}}
 else
 	echo "Config error Check Secrets For Reference check README"
 	exit 2
 	
+if [[ -n ${{secrets.MAX_CONCURRENT_DOWNLOADS}} ]]; then
+	heroku config:set -a ${{secrets.HEROKU_APP}} MAX_CONCURRENT_DOWNLOADS=${{secrets.MAX_CONCURRENT_DOWNLOADS}}
+else
+	echo "Max Concurrent Downloads Var Not given so Defaults to 3"
+fi
 
 echo "Deployment Completed"
