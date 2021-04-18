2b704e1a7edf6a8efce4a7d107d48db4628d371d
Arghya Chanda <arghyac35@gmail.com>
2021-04-18 07:31:07 +0530

    Fix sed command, it was failing on Mac
    * Fix taken from https://stackoverflow.com/questions/4247068/sed-command-with-i-option-failing-on-mac-but-works-on-linux
    
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>

--- start.sh
+++ start.sh
@@ -29,13 +29,17 @@ fi
 
 
 if [[ -n $MAX_CONCURRENT_DOWNLOADS ]]; then
-	sed -i "/max-concurrent-downloads/d" $(pwd)/aria.conf
+	sed -i'' -e "/max-concurrent-downloads/d" $(pwd)/aria.conf
 	echo -e "max-concurrent-downloads=$MAX_CONCURRENT_DOWNLOADS" >> $(pwd)/aria.conf
 fi
 
-sed -i "/bt-tracker=/d" $(pwd)/aria.conf
+sed -i'' -e "/bt-tracker=/d" $(pwd)/aria.conf
 tracker_list=`curl -Ns https://raw.githubusercontent.com/XIU2/TrackersListCollection/master/all.txt | awk '$1' | tr '\n' ',' | cat`
 echo -e "bt-tracker=$tracker_list" >> $(pwd)/aria.conf
 
+# Remove the .bak file got created from above sed
+test -f $(pwd)/aria.conf-e && rm $(pwd)/aria.conf-e
+
 aria2c --conf-path=aria.conf
+echo "Aria2c daemon started"
 npm start
