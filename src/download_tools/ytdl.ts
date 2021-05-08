1a73c12cfb186d4a60b216a187792a5adc5f1d5a
Arghya Chanda <arghyac35@gmail.com>
2021-05-08 23:39:26 +0530

    YTDL: Ability to choose quality
    * Supported quality to pass as input: 1080, 720, 480, 360, 240, audio, by default it will select highest quality for the video.
    
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>

--- src/download_tools/ytdl.ts
+++ src/download_tools/ytdl.ts
@@ -10,6 +10,7 @@ import driveTar = require('../drive/drive-tar');
 import { DlVars } from '../dl_model/detail';
 import fs from 'fs';
 import downloadUtils = require('../download_tools/utils');
+import { prepDownload } from '../index'
 
 const dlDetails: DlVars = {
     isTar: false,
@@ -34,12 +35,60 @@ const dlDetails: DlVars = {
     extractedFileSize: ''
 };
 
-export async function ytdlWrapper(url: string, bot: TelegramBot, tgMsg: TelegramBot.Message, actualMsg: TelegramBot.Message) {
+const supportedQualities = [
+    { quality: '1080', itag: 137, onlyvideo: true }, // video only
+    { quality: '720', itag: 22, onlyvideo: false },
+    { quality: '480', itag: 135, onlyvideo: true }, // video only
+    { quality: '360', itag: 18, onlyvideo: false },
+    { quality: '240', itag: 133, onlyvideo: true }, // video only
+    { quality: 'audio', itag: 'highestaudio', onlyvideo: false },
+];
+
+export async function ytdlWrapper(url: string, bot: TelegramBot, tgMsg: TelegramBot.Message, actualMsg: TelegramBot.Message, quality = '') {
+
     url = url.trim()
     let info = await ytdl.getInfo(url);
 
     let message = `Downloading: <code>${info.videoDetails.title}</code>`;
-    msgTools.editMessage(bot, tgMsg, message);
+    await msgTools.editMessage(bot, tgMsg, message);
+
+    const supportedQuality = supportedQualities.find(data => data.quality === quality.toLowerCase());
+    if (quality) {
+        if (supportedQuality) {
+            const options: ytdl.chooseFormatOptions = { quality: supportedQuality.itag };
+            if (supportedQuality.quality === 'audio') {
+                options.filter = 'audioonly';
+            }
+            const videoformat = ytdl.chooseFormat(info.formats, options);
+            if (!videoformat) {
+                throw new Error(`Quality: ${supportedQuality.quality} not found for this video. You may try downloading it without passing any quality, let ytdl choose the best quality.`);
+            }
+            if (!supportedQuality.onlyvideo) {
+                prepDownload(actualMsg, videoformat.url, false, false, info.videoDetails.title + '.' + videoformat.container);
+                msgTools.deleteMsg(bot, tgMsg);
+            } else {
+                let audioitag: any = '';
+                if (supportedQuality.quality !== '1080') {
+                    const audioformat = ytdl.chooseFormat(info.formats, { quality: 'lowestaudio', filter: 'audioonly' });
+                    if (audioformat) audioitag = audioformat.itag;
+                }
+                downloadSeperateStreamAndMerge(info, bot, tgMsg, actualMsg, videoformat.itag, audioitag);
+            }
+
+        } else {
+            throw new Error(`Quality: ${quality} is not supported.`);
+        }
+    } else {
+        downloadSeperateStreamAndMerge(info, bot, tgMsg, actualMsg);
+    }
+
+}
+
+async function downloadSeperateStreamAndMerge(info: ytdl.videoInfo, bot: TelegramBot, tgMsg: TelegramBot.Message, actualMsg: TelegramBot.Message, quality: any = '', audioitag: any = '') {
+
+    // Prepare the progress bar
+    let progressbarHandle: NodeJS.Timeout = null;
+    const progressbarInterval = constants.STATUS_UPDATE_INTERVAL_MS ? constants.STATUS_UPDATE_INTERVAL_MS : 12000;
 
     const tracker = {
         start: Date.now(),
@@ -48,45 +97,20 @@ export async function ytdlWrapper(url: string, bot: TelegramBot, tgMsg: Telegram
         merged: { frame: 0, speed: '0x', fps: 0 },
     };
 
-    // Get audio and video streams
-    const audio = ytdl(url, { quality: 'highestaudio' })
-        .on('progress', (_, downloaded, total) => {
-            tracker.audio = { downloaded, total };
-        });
-    const video = ytdl(url, { quality: 'highestvideo' })
-        .on('progress', (_, downloaded, total) => {
-            tracker.video = { downloaded, total };
-        });
-
-    // Prepare the progress bar
-    let progressbarHandle: NodeJS.Timeout = null;
-    const progressbarInterval = constants.STATUS_UPDATE_INTERVAL_MS ? constants.STATUS_UPDATE_INTERVAL_MS : 12000;
-    const showProgress = () => {
-        // readline.cursorTo(process.stdout, 0);
-        const toMB = (i: number) => (i / 1024 / 1024).toFixed(2);
-
-        // process.stdout.write(`Audio  | ${(tracker.audio.downloaded / tracker.audio.total * 100).toFixed(2)}% processed `);
-        // process.stdout.write(`(${toMB(tracker.audio.downloaded)}MB of ${toMB(tracker.audio.total)}MB).${' '.repeat(10)}\n`);
-
-        // process.stdout.write(`Video  | ${(tracker.video.downloaded / tracker.video.total * 100).toFixed(2)}% processed `);
-        // process.stdout.write(`(${toMB(tracker.video.downloaded)}MB of ${toMB(tracker.video.total)}MB).${' '.repeat(10)}\n`);
-
-        // process.stdout.write(`Merged | processing frame ${tracker.merged.frame} `);
-        // process.stdout.write(`(at ${tracker.merged.fps} fps => ${tracker.merged.speed}).${' '.repeat(10)}\n`);
-
-        // process.stdout.write(`running for: ${((Date.now() - tracker.start) / 1000 / 60).toFixed(2)} Minutes.`);
-        // readline.moveCursor(process.stdout, 0, -3);
-
-
-        message = `Downloading: <code>${info.videoDetails.title}</code>\nAudio  | ${(tracker.audio.downloaded / tracker.audio.total * 100).toFixed(2)}% processed (${toMB(tracker.audio.downloaded)}MB of ${toMB(tracker.audio.total)}MB).${' '.repeat(10)}\nVideo  | ${(tracker.video.downloaded / tracker.video.total * 100).toFixed(2)}% processed (${toMB(tracker.video.downloaded)}MB of ${toMB(tracker.video.total)}MB).${' '.repeat(10)}\nMerged | processing frame ${tracker.merged.frame} (at ${tracker.merged.fps} fps => ${tracker.merged.speed}).${' '.repeat(10)}\nrunning for: ${((Date.now() - tracker.start) / 1000 / 60).toFixed(2)} Minutes.`;
-
-        msgTools.editMessage(bot, tgMsg, message);
-    };
-
     let dlDir = uuidv4();
     let realFilePath = `${constants.ARIA_DOWNLOAD_LOCATION}/${dlDir}/${info.videoDetails.title}.mkv`;
 
     fs.mkdirSync(`${constants.ARIA_DOWNLOAD_LOCATION}/${dlDir}`, { recursive: true });
+
+    // Get audio and video streams
+    const audio = ytdl.downloadFromInfo(info, { quality: audioitag ? audioitag : 'highestaudio' })
+        .on('progress', (_, downloaded, total) => {
+            tracker.audio = { downloaded, total };
+        });
+    const video = ytdl.downloadFromInfo(info, { quality: quality ? quality : 'highestvideo' })
+        .on('progress', (_, downloaded, total) => {
+            tracker.video = { downloaded, total };
+        });
 
     // Start the ffmpeg child process
     const ffmpegProcess: any = cp.spawn(ffmpeg, [
@@ -128,12 +152,11 @@ export async function ytdlWrapper(url: string, bot: TelegramBot, tgMsg: Telegram
         clearInterval(progressbarHandle);
     });
 
-    console.log('called-->231');
     // Link streams
     // FFmpeg creates the transformer streams and we just have to insert / read data
     ffmpegProcess.stdio[3].on('data', (chunk: any) => {
         // Start the progress bar
-        if (!progressbarHandle) progressbarHandle = setInterval(showProgress, progressbarInterval);
+        if (!progressbarHandle) progressbarHandle = setInterval(showProgress(info, tracker, bot, tgMsg), progressbarInterval);
         // Parse the param=value list returned by ffmpeg
         const lines = chunk.toString().trim().split('\n');
         const args: any = {};
@@ -143,12 +166,32 @@ export async function ytdlWrapper(url: string, bot: TelegramBot, tgMsg: Telegram
         }
         tracker.merged = args;
     });
-    console.log('called-->45');
     audio.pipe(ffmpegProcess.stdio[4]);
     video.pipe(ffmpegProcess.stdio[5]);
-    console.log('called-->56');
-
 }
+
+const showProgress = (info: ytdl.videoInfo, tracker: any, bot: TelegramBot, tgMsg: TelegramBot.Message) => () => {
+    // readline.cursorTo(process.stdout, 0);
+    const toMB = (i: number) => (i / 1024 / 1024).toFixed(2);
+
+    // process.stdout.write(`Audio  | ${(tracker.audio.downloaded / tracker.audio.total * 100).toFixed(2)}% processed `);
+    // process.stdout.write(`(${toMB(tracker.audio.downloaded)}MB of ${toMB(tracker.audio.total)}MB).${' '.repeat(10)}\n`);
+
+    // process.stdout.write(`Video  | ${(tracker.video.downloaded / tracker.video.total * 100).toFixed(2)}% processed `);
+    // process.stdout.write(`(${toMB(tracker.video.downloaded)}MB of ${toMB(tracker.video.total)}MB).${' '.repeat(10)}\n`);
+
+    // process.stdout.write(`Merged | processing frame ${tracker.merged.frame} `);
+    // process.stdout.write(`(at ${tracker.merged.fps} fps => ${tracker.merged.speed}).${' '.repeat(10)}\n`);
+
+    // process.stdout.write(`running for: ${((Date.now() - tracker.start) / 1000 / 60).toFixed(2)} Minutes.`);
+    // readline.moveCursor(process.stdout, 0, -3);
+
+
+    let message = `Downloading: <code>${info.videoDetails.title}</code>\nAudio  | ${(tracker.audio.downloaded / tracker.audio.total * 100).toFixed(2)}% processed (${toMB(tracker.audio.downloaded)}MB of ${toMB(tracker.audio.total)}MB).${' '.repeat(10)}\nVideo  | ${(tracker.video.downloaded / tracker.video.total * 100).toFixed(2)}% processed (${toMB(tracker.video.downloaded)}MB of ${toMB(tracker.video.total)}MB).${' '.repeat(10)}\nMerged | processing frame ${tracker.merged.frame} (at ${tracker.merged.fps} fps => ${tracker.merged.speed}).${' '.repeat(10)}\nrunning for: ${((Date.now() - tracker.start) / 1000 / 60).toFixed(2)} Minutes.`;
+
+    msgTools.editMessage(bot, tgMsg, message);
+};
+
 
 async function startUpload(dlDir: string, file: string, filename: string, bot: TelegramBot, tgMsg: TelegramBot.Message, actualMsg: TelegramBot.Message, message: string) {
     message += `\n\n✔Video download complete, starting file upload...`;
